23e78f824ef955e689f67d35a0216c4b2215a06a
facudemarco <fac.demarco37@gmail.com>
2025-06-18 14:48:40 -0300

    void

--- schemas/token.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-class TokenCreate(BaseModel):
-    email: str
-    password: str
--- schemas/user.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class UserCreate(BaseModel):
-    email: str
-    password: str
-
-class UserUpdate(BaseModel):
-    email: str
-    password: str
