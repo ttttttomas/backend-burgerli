078d2bd9d0b8759f501a74be7198559e75a9d933
Facundo <fac.demarco37@gmail.com>
2025-08-05 15:40:46 -0300

    Add microService

---
+++ mercadoPagoMicroService/controllers/payment.controller.js
@@ -0,0 +1,26 @@
+import { MercadoPagoConfig, Preference } from 'mercadopago'
+
+const MP_ACCESS_TOKEN= process.env.MP_ACCESS
+ 
+export const createOrder = async (req, res) => {
+const client = new MercadoPagoConfig({accessToken: MP_ACCESS_TOKEN})
+
+const preference = new Preference(client)
+
+try {
+   const response = await preference.create({
+        body: {
+           ...req.body,
+            redirect_urls: {
+                success: 'https://www.mercadopago.com.ar/mp-assets/img/success.png',
+                failure: 'https://www.mercadopago.com.ar/mp-assets/img/failure.png',
+                pending: 'https://www.mercadopago.com.ar/mp-assets/img/pending.png'
+            },
+        }
+    })
+    res.json(response)
+    console.log(response)
+}
+catch (error) {
+    console.log(error)
+}}
