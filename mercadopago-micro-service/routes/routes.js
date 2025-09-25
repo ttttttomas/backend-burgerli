bba954fb15134506e9b74cc7e97f883d04bf7b97
Facundo <fac.demarco37@gmail.com>
2025-09-25 15:43:36 -0300

    Delete microservice

--- mercadopago-micro-service/routes/routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import {createOrder} from "../controllers/payment.controller.js";
-
-const router = Router();
-
-router.post("/create-order", createOrder);
-
-export default router;
