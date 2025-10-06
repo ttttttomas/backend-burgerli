c1959712127eadbe3b614b6ba7cb3ff1b8b6c412
ttttttomas <156537417+ttttttomas@users.noreply.github.com>
2025-10-06 15:20:53 -0300

    Update origins main.py

--- main.py
+++ main.py
@@ -12,10 +12,9 @@ app = FastAPI(root_path="/api")
 
 origins = [
     "http://localhost:3000",
-    "http://localhost:3001",
+    "https://imido-curliest-cole.ngrok-free.dev",
     "https://cart-test-nu.vercel.app",
     "http://127.0.0.1:5500",
-    "localhost:5500",
     "http://localhost:5500",
     "https://facudemarco.github.io",
     "https://api-burgerli.iwebtecnology.com",
